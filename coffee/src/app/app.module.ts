2c28c7ca53b02fbb1fe79a8c9ff8c73c6d7d9907
Sondre Kristensen <Sondre@dhcp-10-22-33-43.wlan.ntnu.no>
2016-11-13 11:46:03 +0100

    Navbar component

--- coffee/src/app/app.module.ts
+++ coffee/src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
