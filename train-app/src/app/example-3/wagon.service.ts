1af996155d7e3275a39d6f114c4739290fa23d04
tamhuy <quangtamh95@gmail.com>
2016-11-18 14:53:32 +0100

    asd

--- train-app/src/app/example-3/wagon.service.ts
+++ train-app/src/app/example-3/wagon.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Subject }    from 'rxjs/Subject';
 
 @Injectable()
 export class WagonService {
 
-  constructor() { }
 
   //TODO: Write your code here
-  
+
 }
